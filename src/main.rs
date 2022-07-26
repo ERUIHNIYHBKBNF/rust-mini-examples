9247e8e7662a567bce79b6fdb808c6e540bcce84
ERUIHNIYHBKBNF <839203521@qq.com>
2022-07-26 17:01:52 +0800

    data type

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,18 @@ fn main() {
     println!("Hello {}!", a);
     a = "Kafuu Chino";
     println!("Hello {}!", a);
+
+    let a: u32 = 233;
+    let b = 233u64;
+    println!("a = {},  b = {}", a, b);
+    
+    let is_bigger: bool = u64::from(a) > b;
+    println!("a is bigger than b: {}", is_bigger);
+
+    let varchar: char = '😀';
+    let str1: &str = "Hello,{}";
+    // TODO: println!(str1, varchar);
+    println!("{}{{{}}}", str1, varchar); // Hello,{}{😀}
+    
+    
 }
