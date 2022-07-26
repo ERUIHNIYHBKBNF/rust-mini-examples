666707938f7070b7c6a70511f1ba587b10fecc70
ERUIHNIYHBKBNF <839203521@qq.com>
2022-07-27 00:37:51 +0800

    array&vector

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-fn divide_by_5(num: u32) -> u32 {
-	// 使函数中的最后一行代码等于要返回的值，从而在函数末尾返回一个值
-    num / 5
-}
-
 fn main() {
-    let num = 25;
-    println!("{} divided by 5 = {}", num, divide_by_5(num));
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    println!("days: {:?}", days);
+    // Declare array, initialize all values to 0, length = 5
+    let bytes: [u32; 5] = [0; 5];
+    println!("bytes[4]: {:?}", bytes[4]);
+
+    let vector = vec![1, 2, 3, 4u64, 5];
+    println!("vector: {:?}", vector);
+    // 可变vector
+    let mut fruits = Vec::new();
+    fruits.push("Apple");
+    fruits.push("Banana");
+    fruits.pop();
+    fruits.push("Cherry");
+    let mut cherry = fruits[1].to_string();
+    cherry += "s";
+    fruits[1] = &cherry;
+    println!("fruits[1]: {}", fruits.pop().unwrap());
 }
