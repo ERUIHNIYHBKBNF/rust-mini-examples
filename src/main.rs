2111fcd1b7afb573ab33b9bb8b2a0759fc3d73cf
ERUIHNIYHBKBNF <839203521@qq.com>
2022-07-26 21:29:33 +0800

    enum

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,6 @@ fn main() {
 
     let tuple = ("chino", 1204, true);
     println!("({}, {}, {})", tuple.0, tuple.1, tuple.2);
-
     // <struct>.<field>
     struct Student {
         name: String,
@@ -37,6 +36,50 @@ fn main() {
     let grades: Grades = Grades('A', 'B', 'C', 'D', 0.9);
     print!("name: {}, level: {}, remote: {} ", student.name, student.level, student.remote);
     println!("{}", grades.0);
+
+    /**
+     * 通过 #[derive(Debug)] 语法可以在代码执行期间查看某些在标准输出中无法查看的值。
+     * 要使用 println! 宏查看调试数据，请使用语法 {:#?} 以可读的方式格式化数据。
+     */
+    #[derive(Debug)]
+    struct ColorProps {
+        red: f32,
+        green: f32,
+        blue: f32,
+    }
+    #[derive(Debug)]
+    struct ColorTuple(f32, f32, f32);
+    /** An enum variant can be like a unit without fields or data types / tuple / classic struct
+     * 枚举中的每个变体均不是其自己的类型, 任何使用枚举变体的函数都必须接受枚举中的所有变体
+     * 通过为枚举中的每个变量定义单独的结构，可以直接访问特定变量的数据。
+     * 感觉enum就像存储一堆struct的类型（？）
+     */
+    #[derive(Debug)]
+    enum Color {
+        Hex(String),
+        RgbTuple(ColorTuple),
+        RgbStruct(ColorProps),
+    }
+
+    let red_str: String = "#ff0000".to_string();
+    println!("red({})", red_str);
+    let red: Color = Color::Hex(red_str);
+
+    let green_tuple = ColorTuple(0.0, 1.0, 0.0);
+    println!("green({}, {}, {})", green_tuple.0, green_tuple.1, green_tuple.2);
+    let green: Color = Color::RgbTuple(green_tuple);
+
+    let blue_struct: ColorProps = ColorProps {
+        red: 0.0,
+        green: 0.0,
+        blue: 1.0,
+    };
+    println!("blue({}, {}, {})", blue_struct.red, blue_struct.green, blue_struct.blue);
+    let blue: Color = Color::RgbStruct(blue_struct);
+
+    // Use the {:#?} syntax to display the enum structure and data in a readable form
+    println!("enum:\nred: {:#?}\ngreen: {:#?}\nblue: {:#?}", red, green, blue);
+
 
     
 }
